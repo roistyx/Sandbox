6f733e71d5992ba61ed7c67eaeb7749d6cb1d61e
Roistyx <109058904+roistyx@users.noreply.github.com>
2022-08-21 18:30:17 +0300

    v0

---
+++ script/script.js
@@ -0,0 +1,19 @@
+// var x = 2;
+// // var y = 5;
+
+var result  = document.getElementById ('result');
+var x_value = document.getElementById ('x_value');
+console.log(x_value.value)
+
+// function write_result (){
+//     if (x_value>4) {
+//     // result.innerHTML = x_value.value; 
+//     console.log(x_value.value)
+//     }
+// }
+
+
+    // x_value.addEventListener('click', write_result)
+
+
+    
