1dbbd49ecc3d12080677c4a554d8311b3ddca688
Your Name <you@example.com>
2022-08-24 22:29:52 -0400

    stop watch

--- script/script.js
+++ script/script.js
@@ -3,14 +3,8 @@
 
 var result  = document.getElementById ('result');
 var x_value = document.getElementById ('x_value');
-console.log(x_value.value)
 
-// function write_result (){
-//     if (x_value>4) {
-//     // result.innerHTML = x_value.value; 
-//     console.log(x_value.value)
-//     }
-// }
+
 
 
     // x_value.addEventListener('click', write_result)
