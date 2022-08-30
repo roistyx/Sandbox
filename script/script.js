b0bda5f61908dfc0518921d94d6b022530d425ab
Roistyx <109058904+roistyx@users.noreply.github.com>
2022-08-30 15:07:31 +0300

    everything

--- script/script.js
+++ script/script.js
@@ -1,5 +1,15 @@
-function get_result() {
-  var x_value = document.getElementById ('x_value').value;
-  console.log(x_value)
-  document.getElementById("result").innerHTML = x_value;
-}
+var form_submit = document.getElementById('form-submit');
+// console.log(form_submit.id);
+form_submit.addEventListener("click", get_result);
+
+    function get_result(){
+    var x_value = document.getElementById('x-value');
+    var feedback = document.getElementById('feedback');
+    feedback.innerHTML = x_value.value;
+
+    // console.log(x_value.value);
+    
+      // document.getElementById("result").innerHTML = "Enter number";
+    }
+    
+    
