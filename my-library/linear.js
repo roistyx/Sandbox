38429dbc5fb3a0f02e5686eb3801dcd05c2591d6
Roistyx <109058904+roistyx@users.noreply.github.com>
2022-09-11 10:08:56 +0300

    Added Shay's Binary search

--- my-library/linear.js
+++ my-library/linear.js
@@ -5,5 +5,10 @@ function linear_example () {
     }
     const a1 = performance.now()
     console.log(a1)
+    my_array.forEach((item)=>{
+        console.log(item);
+    });
+    const a2 = performance.now();
+    console.log("Time: ", a2 - a1);
 }
 linear_example ()
--- my-library/temp.js
+++ my-library/temp.js
@@ -8,17 +8,36 @@
 // linearExample();
 
 
-function linear_example(){
-    const myArray = [];
-    for (let i = 0; i < 1000; i++) {
-    myArray.push(i);
-    }
-    const a1 = performance.now();
-    myArray.forEach((item) => {
-    console.log(item);
-    });
-    const a2 = performance.now() ;
-    console.log ("Time: ", a2 - a1);
-    }
+// function linear_example(){
+//     const myArray = [];
+//     for (let i = 0; i < 1000; i++) {
+//     myArray.push(i);
+//     }
+//     const a1 = performance.now();
+//     myArray.forEach((item) => {
+//     console.log(item);
+//     });
+//     const a2 = performance.now() ;
+//     console.log ("Time: ", a2 - a1);
+//     }
 
-    linear_example()
+//     linear_example()
+
+function binarySearch(items, term, depth) {
+    const mid = Math.round (items.length / 2);
+    if (items[mid] == term) {
+    console. log("our term is ", items[mid], " it took"+ depth + " iterations");
+    return items.mid;
+    } else if (term > items [mid]) {
+    const newItems = items.slice(mid);
+    return binarySearch(newItems, term, ++depth);
+    } else if (term < items [mid]) {
+        const newItems = items.slice(0, mid);
+        return binarySearch(newItems, term, ++depth);
+        }
+    }
+    const myArr = []
+    for (let i = 0; i < 1000000; i++) {
+    myArr.push(i);
+    }
+    binarySearch(myArr, 999999, 0) ;
