bb83b1c20e9839b7d1df5a3561b7548650e800ce
Tipo <tipo@pop-os.localdomain>
2022-10-06 11:49:17 +0300

    Added file

---
+++ my-library/for-loop.js
@@ -0,0 +1,7 @@
+const array = [1,2,3,4,5,6,7,8,9];
+console.log("Array length", array.length)
+
+for(let i = array.length - 1; i >= 0; i--){
+    // console.log(array.length - 1)
+    console.log(array[i])
+}
